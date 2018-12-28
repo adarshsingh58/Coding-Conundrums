e4f1d462a0ce0f5bed2ae524404f3aaa8133881b
Adarsh Singh <Adarshs@nextgen.com>
2018-12-28 07:56:51 +0530

    kth permutation

---
+++ src/com/math/KthPermutation.java
@@ -0,0 +1,27 @@
+package com.math;
+
+/**
+ * Given a set of n elements find their kth permutation. Consider the following
+ * set of elements:
+ * inputArr: 1,2,3
+ * Output:
+ * 	1st-123
+ * 	2nd-123
+ * 	3rd-123
+ * 	4th-123
+ * 	5st-123
+ * 	6th-123
+ * 
+ */
+public class KthPermutation {
+	public static void main(String[] args) {
+		int k=5;
+		int output=findKthPermutation(k);
+	}
+
+	private static int findKthPermutation(int k) {
+		
+		return 0;
+	}
+
+}
