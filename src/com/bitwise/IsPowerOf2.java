0f295ccd85ca21a54e8ad18e60c614db936050e3
Adarsh Singh <Adarshs@nextgen.com>
2019-01-11 15:00:06 +0530

    new prog added

--- src/com/bitwise/IsPowerOf2.java
+++ src/com/bitwise/IsPowerOf2.java
@@ -2,4 +2,21 @@ package com.bitwise;
 
 public class IsPowerOf2 {
 
+	public static void main(String[] args) {
+		int n=32;
+		boolean isPowOf2= isPowOf2(n);
+		System.out.println(isPowOf2);
+	}
+
+	/**
+	 * Any number which is a power of 2 will always have only 1 set bit in its 
+	 * binary representation.
+	 */
+	private static boolean isPowOf2(int n) {
+		int countofSetBits=CountSetBitsInAnInteger.countSetBits(n);
+		if(countofSetBits==1) {
+			return true;
+		}
+		return false;
+	}
 }
