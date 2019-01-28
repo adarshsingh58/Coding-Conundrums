4742858bf21b63f97659b2dbc17531a8ebc04a04
Adarsh Singh <Adarshs@nextgen.com>
2019-01-28 12:42:08 +0530

    integer reverse added

--- src/Test.java
+++ src/Test.java
@@ -2,39 +2,4 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Test {
-	
-	public static void main(String[] args) {
-	
-		String input1="aaaabbbb";
-		String input2="ab";
-		
-		boolean hasSameC = hasSameChar(input1,input2);
-		System.out.println(hasSameC);
-	}
-
-	private static boolean hasSameChar(String input1, String input2) {
-		Map<Character,Integer> map=new HashMap<>();
-		
-		for(char c:input1.toCharArray()) {
-			if(!map.containsKey(c)) {
-				map.put(c, 1);
-			}
-		}
-		
-		for(char c:input2.toCharArray()) {
-			if(map.containsKey(c)) {
-				map.put(c, -1);
-			}else {
-				return false;
-			}
-		}
-		for(Map.Entry<Character, Integer> e:map.entrySet()) {
-			if(e.getValue()!=-1) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	
 }
