4df9d8de67b8548ee34ae33204318efa6094beae
Adarsh Singh <Adarshs@nextgen.com>
2019-02-06 10:16:49 +0530

    misc folder removed

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Test {
+
+	protected void hello(Number n) {
+		System.out.println("Super hello");
+	}
+
+}
+
+class child extends Test {
+	public void hello(Number n) {
+		System.out.println("Super hello");
+	}
+	
 }
