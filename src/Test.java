22b1ece16a50b3c178c3bab1c657434a0a6a609d
Adarsh Singh <Adarshs@nextgen.com>
2019-02-21 08:24:41 +0530

    math problems added

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,100 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Test {
+	private static List<Kingdom> allies = new ArrayList<>();
+	private static List<String> inputList = new ArrayList<>();
 
-	protected void hello(Number n) {
-		System.out.println("Super hello");
+	public static void main(String[] args) {
+		printOutput();
+
+		provideInput();
+		for (String s : inputList) {
+			String[] message = s.split(", ");
+			messageFromShanTo(Kingdom.valueOf(message[0]), message[1]);
+		}
+		printOutput();
+	}
+
+	private static void provideInput() {
+		System.out.println("Enter Number of inputs");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println("Input Messages to kingdoms from King Shan:");
+		scanner.nextLine();
+		for (int i = 0; i < n; i++) {
+			inputList.add(scanner.nextLine());
+		}
+		scanner.close();
+	}
+
+	private static void printOutput() {
+		System.out.println("Who is the ruler of Southeros?");
+		if (allies.size() >= (Kingdom.values().length/2)) {
+			System.out.println("King Shan");
+		} else {
+			System.out.println("None");
+		}
+		System.out.println("Allies of Ruler?");
+		StringBuilder builder=new StringBuilder();
+		if (allies.size() >= (Kingdom.values().length/2)) {
+			for (Kingdom k : allies) {
+				builder.append(k.name() + ", ");
+			}
+			System.out.print(builder.substring(0, builder.length()-2).toString());
+		} else {
+			System.out.println("None");
+		}
+	}
+
+	public static void messageFromShanTo(Kingdom kingdom, String message) {
+		if (isRightSecretMessage(kingdom.getEmblem().toLowerCase(), message.toLowerCase())) {
+			allies.add(kingdom);
+		}
+	}
+
+	private static boolean isRightSecretMessage(String emblem, String message) {
+		Map<Character, Integer> wordMap = new HashMap<>();
+		for (char c : message.toCharArray()) {
+			if (wordMap.containsKey(c)) {
+				wordMap.put(c, wordMap.get(c) + 1);
+			} else {
+				wordMap.put(c, 1);
+			}
+		}
+
+		for (char c : emblem.toCharArray()) {
+			if (!wordMap.containsKey(c)) {
+				return false;
+			} else {
+				wordMap.put(c, wordMap.get(c) - 1);
+				if (wordMap.get(c) == 0) {
+					wordMap.remove(c);
+				}
+			}
+		}
+		return true;
 	}
 
 }
 
-class child extends Test {
-	public void hello(Number n) {
-		System.out.println("Super hello");
+enum Kingdom {
+	Air("Owl"), Land("Panda"), Ice("Mammoth"), Water("Octopus"), Fire("Dragon"), Space("Gorilla");
+
+	private String emblem;
+
+	private Kingdom() {
 	}
-	
+
+	Kingdom(String emblem) {
+		this.emblem = emblem;
+	}
+
+	public String getEmblem() {
+		return emblem;
+	}
+
 }
---
+++ src/com/scenarioBasedQues/DistanceBet2Points.java
@@ -0,0 +1,44 @@
+package com.scenarioBasedQues;
+
+import java.awt.geom.Point2D;
+
+/**
+ * if co ordinates of two points are x1,y1 and x2,y2, find the distance between
+ * them in a plane.
+ * 
+ * distance= SQUAREROOT{(y2-y2)^2+(x2-x2)^2}
+ */
+public class DistanceBet2Points {
+
+	public static void main(String[] args) {
+		double x1 = 3;
+		double y1 = 4;
+		double x2 = 7;
+		double y2 = 1;
+		
+		double result1=calculateDistanceBetweenPointsWithHypot(x1, y1, x2, y2);
+		double result2=calculateDistanceBetweenPointsWithHypot(x1, y1, x2, y2);
+		System.out.println(result1+" "+result2);
+		
+	}
+
+	public static double calculateDistanceBetweenPointsWithHypot(double x1, double y1, double x2, double y2) {
+
+		double ac = Math.abs(y2 - y1);
+		double cb = Math.abs(x2 - x1);
+
+		return Math.hypot(ac, cb);
+	}
+	
+	/**
+	 * Here we will use Point2D library from java.awt.geom.Point2D Package
+	 */
+	public double calculateDistanceBetweenPointsWithPoint2D( 
+		    double x1, 
+		    double y1, 
+		    double x2, 
+		    double y2) {
+		         
+		    return Point2D.distance(x1, y1, x2, y2);
+		}
+}
