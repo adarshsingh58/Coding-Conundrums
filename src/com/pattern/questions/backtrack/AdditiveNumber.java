fe45a79a96dcd73e7c55d6726482aec9f319fc39
adarsh.singh <adarsh.singh@salesforce.com>
2025-03-19 21:46:40 +0530

    fixed structure

--- src/com/pattern/questions/backtrack/AdditiveNumber.java
+++ src/com/pattern/questions/backtrack/AdditiveNumber.java
@@ -1,30 +1,69 @@
 package com.pattern.questions.backtrack;
-/**An additive number is a string whose digits can form an additive sequence.
 
- A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
-
- Given a string containing only digits, return true if it is an additive number or false otherwise.
-
- Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
-
-
-
- Example 1:
-
- Input: "112358"
- Output: true
- Explanation:
- The digits can form an additive sequence: 1, 1, 2, 3, 5, 8.
- 1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
-
- Example 2:
-
- Input: "199100199"
- Output: true
- Explanation:
- The additive sequence is: 1, 99, 100, 199.
- 1 + 99 = 100, 99 + 100 = 199
-
- https://leetcode.com/problems/additive-number/description/*/
+/**
+ * An additive number is a string whose digits can form an additive sequence.
+ * <p>
+ * A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent
+ * number in the sequence must be the sum of the preceding two.
+ * <p>
+ * Given a string containing only digits, return true if it is an additive number or false otherwise.
+ * <p>
+ * Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "112358" Output: true Explanation: The digits can form an additive sequence: 1, 1, 2, 3, 5, 8. 1 + 1 = 2, 1 +
+ * 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: "199100199" Output: true Explanation: The additive sequence is: 1, 99, 100, 199. 1 + 99 = 100, 99 + 100 = 199
+ * <p>
+ * https://leetcode.com/problems/additive-number/description/
+ */
 public class AdditiveNumber {
+    public static void main(String[] args) {
+        String inp = "199100199";
+        boolean isAdditive = isAdditive(inp);
+        System.out.println(isAdditive);
+    }
+
+    private static boolean isAdditive(String inp) {
+        for (int i = 1; i < inp.length(); i++) {
+            for (int j = i; j < inp.length(); j++) {
+                if (isAdditiveRec(inp, 0, i, j, false))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isAdditiveRec(String inpArr, int firstStart, int firstEndDIndexelta, int secondEndIndexDelta, boolean found) {
+        if (firstStart + firstEndDIndexelta + secondEndIndexDelta == inpArr.length()) {
+            found = true;
+            return true;
+        }
+        int firstNum = formNumber(inpArr, firstStart, firstEndDIndexelta);
+        int secondNum = formNumber(inpArr, firstEndDIndexelta + 1, secondEndIndexDelta);
+        int thirdIndexDelta=findLength(firstNum+secondNum);
+        int thirdNum = formNumber(inpArr, secondEndIndexDelta + 1, thirdIndexDelta);
+        if (firstNum + secondNum != thirdNum) {
+            found = false;
+            return false;
+        }
+        return isAdditiveRec(inpArr, firstStart + firstEndDIndexelta, firstEndDIndexelta, secondEndIndexDelta, found);
+    }
+
+    private static int findLength(int num) {
+        int count=0;
+        while(num!=0){
+            num=num/10;
+            count++;
+        }
+        return count;
+    }
+
+    private static int formNumber(String inpArr, int firstStart, int firstEndDIndexelta) {
+        return Integer.valueOf(inpArr.substring(firstStart,firstStart+firstEndDIndexelta));
+    }
 }
--- src/com/pattern/questions/backtrack/CoinChange_I.java
+++ src/com/pattern/questions/backtrack/CoinChange_I.java
@@ -1,7 +1,10 @@
 package com.pattern.questions.backtrack;
 
 import java.util.*;
-
+/**
+ * * CoinChange_I: This is the first variation of the Coin Change Problem. In this variation, you're asked to return
+ * the minimum number of coins needed to make the change amount.
+ */
 public class CoinChange_I {
 
     public static void main(String[] args) {
@@ -14,26 +17,49 @@ public class CoinChange_I {
 
         // should be 3
         System.out.println(CoinChange_I.minCoinsRequired(45, new ArrayList<>(Arrays.asList(1, 5, 10, 25))));
-
         // should be 5
         System.out.println(CoinChange_I.minCoinsRequired(23, new ArrayList<>(Arrays.asList(1, 5, 10, 25))));
 
         // should be 8
-        System.out.println(CoinChange_I.minCoinsRequired(74, new ArrayList<>(Arrays.asList(1, 5, 10, 25))));
+        System.out.println(CoinChange_I.minCoinsRequired(75, new ArrayList<>(Arrays.asList(1, 5, 10, 25))));
     }
 
     private static int minCoinsRequired(int target, List<Integer> coins) {
-        return minCoinsRequired(target, coins, new HashMap<>());
+//        return minCoinsRequired_whenOnly1CoinofEachCanBeUsed(target, coins, new HashMap<>());
+
+        List<List<Integer>> opMain = new ArrayList<>();
+        List<Integer> op = new ArrayList<>();
+        minCoinsRequired_whenAnyNumOfCoinofEachCanBeUsed(target, coins, coins.size() - 1, op,opMain);
+        opMain.forEach(h->h.forEach( y->{
+            System.out.print(y+" ");
+        }));
+        System.out.println();
+        return 0;
     }
 
-    /**
-     * * CoinChange_I: This is the first variation of the Coin Change Problem. In this variation, you're asked to return
-     * the minimum number of coins needed to make the change amount.
-     */
+    private static boolean minCoinsRequired_whenAnyNumOfCoinofEachCanBeUsed(int target, List<Integer> coins, int index, List<Integer> coinsConsideredSofar,List<List<Integer>> opMain) {
+        if (target < 0)
+            return false;
+        if (target == 0) {
+            opMain.add(coinsConsideredSofar);
+            return true;
+        }
+        coinsConsideredSofar.add(coins.get(index));
+        if (minCoinsRequired_whenAnyNumOfCoinofEachCanBeUsed(target - coins.get(index), coins, index, coinsConsideredSofar, opMain)) {
+            return true;
+        } else {
+            coinsConsideredSofar.remove(coinsConsideredSofar.size() - 1);
+            index--;
+            return minCoinsRequired_whenAnyNumOfCoinofEachCanBeUsed(target, coins, index , coinsConsideredSofar, opMain);
+        }
+    }
 
-    // ? Efficient solution, as it uses DP
-    // ? to store the previously calculated results
-    private static int minCoinsRequired(int target, List<Integer> coins, Map<Integer, Integer> results) {
+
+    /**
+     * Here of the given coins, only 1 of each can be used to get the final target. Basically for a given array we need
+     * to find if any subset adds up to the target Sum.
+     */
+    private static int minCoinsRequired_whenOnly1CoinofEachCanBeUsed(int target, List<Integer> coins, Map<Integer, Integer> results) {
 
         int minimumCoins = target;
 
@@ -45,7 +71,7 @@ public class CoinChange_I {
         else
             for (Integer coin : coins)
                 if (coin <= target) {
-                    int numberOfCoins = 1 + minCoinsRequired(target - coin, coins, results);
+                    int numberOfCoins = 1 + minCoinsRequired_whenOnly1CoinofEachCanBeUsed(target - coin, coins, results);
 
                     if (numberOfCoins < minimumCoins) {
                         minimumCoins = numberOfCoins;
